0f1a645525cc30b6b349f521cd0ca8ab8f9a6dfb
Manasa Potnuru <manasa.potnuru@appsassociates.com>
2025-09-26 11:30:38 +0000

    code combination

---
+++ models/consumption/gl_code_combinations.sql
@@ -0,0 +1,10 @@
+{{
+    config(materialzied="incremental",unique_key="integration_id")
+}}
+
+select 
+    *,code_combination_id as integration_id
+from{{
+    source('ebs','gl_code_combinations')
+}}
+
