c215fdae066993d38a3486ba9ec1cb4f4dfaea61
Manasa Potnuru <manasa.potnuru@appsassociates.com>
2025-09-26 17:03:26 +0000

    jhg

--- models/Consumption/fnd_flex_values_vl.sql
+++
@@ -1,6 +0,0 @@
-{{ config(materialized="view", unique_key="integration_id") }}
-
-select
-  *,
-  flex_value_id as integration_id
-from {{ source("ebs", "fnd_flex_values_vl") }}
--- models/Consumption/gl_je_sources.sql
+++
@@ -1,6 +0,0 @@
-{{ config(materialized="view", unique_key="integration_id") }}
-
-select
-  *,
-  je_source_name as integration_id
-from {{ source("ebs", "gl_je_sources") }}
