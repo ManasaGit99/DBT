c4b92612058196cdc555eb22e99762e24038ca1a
Manasa Potnuru <manasa.potnuru@appsassociates.com>
2025-09-26 12:59:35 +0000

    project yaml change

--- models/consumption/gl_code_combinations.sql
+++ models/consumption/gl_code_combinations.sql
@@ -1,10 +1,8 @@
-{{
-    config(materialzied="incremental",unique_key="integration_id")
-}}
 
-select 
-    *,code_combination_id as integration_id
-from{{
-    source('ebs','gl_code_combinations')
-}}
+{{ config(materialized="incremental", unique_key="integration_id") }}
+ 
+select
+  *,
+  code_combination_id as integration_id
+from {{ source("ebs", "gl_code_combinations") }}
 
